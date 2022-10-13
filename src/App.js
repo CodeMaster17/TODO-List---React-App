09a6d6a6df3118ae7d267202798fb48c7fb1f4c9
Harshit <2105891@kiit.ac.in>
2022-10-13 22:56:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
-
 function App() {
+
+  const [inputList, setinputList] = useState("");
+  const [item, setItem] = useState([]);
+
+  const itemAdd = (e) => {
+    setinputList(e.target.value);
+  };
+
+  const listOfItems = () => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="outerDiv">
+        <div className="centerDiv">
+          <br />
+          <h2>ToDo List</h2>
+
+          <div className="itemAdd">
+
+            <input type="text" placeholder='Add item' onChange={itemAdd} />
+            <button className="additionButton" onClick={listOfItems}> + </button>
+          </div>
+
+          <div className="mainArea">
+            <ol>
+              <li>
+                {item.map((itemval) => {
+                  return <li> {itemval}</li>
+                })}
+              </li>
+            </ol>
+          </div>
+
+
+        </div>
+      </div>
+    </>
   );
 }
 
